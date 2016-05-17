d67d87f45496c24616876ecc0a56d8f8ba46dbf2
Jamiu Abdulmujeeb <abdulmujeeb.jamiu@yahoo.com>
2016-05-17 15:59:30 +0100

    Add createNote and listNote methods

--- noteApplication.js
+++ noteApplication.js
@@ -1,4 +1,22 @@
-function NoteApplication(author){
-	this.author=author
-	this.list=
+function NoteApplication(){
+	
 }
+
+
+var newPerson = new NoteApplication(author);
+	newPerson.author=author;
+	newPerson.notes=[];
+
+this.create = function(note_content){
+	this.notes.push(note_content);
+}
+
+
+this.listNotes = function(){
+	listOfNotes = []
+	for (var i=0; i < this.notes.length; i++)
+		listNotes.push(this.notes[i]);
+	return listNotes
+}
+
+
