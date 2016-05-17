24797cf2e59d2d03a8a52417b37296d0e03f9953
Jamiu Abdulmujeeb <abdulmujeeb.jamiu@yahoo.com>
2016-05-17 22:52:16 +0100

    DRYing the code

--- noteApplication.js
+++ noteApplication.js
@@ -1,12 +1,14 @@
 function NotesApplication(author){
 	this.author=author;
 	this.notes=[];
-
-
 	line="-------***************-----------"
+
+
+
 	this.create = function(note_content){
-		if (note_content.length>0){
+		if (note_content.length > 0){
 			this.notes.push(note_content);
+			console.log("Your new note has been added!"
 		} else {
 			console.log("Your note is empty");
 		}
@@ -25,36 +27,52 @@ function NotesApplication(author){
 
 
 	this.getNote = function(note_id){
-		console.log("Note ID: " + [note_id] + "\n" + this.notes[note_id] + "\n\nBy Author: " + this.author);
+		if (note_id > -1 && note_id < this.notes.length){
+			console.log("Note ID: " + [note_id] + "\n" + this.notes[note_id] + "\n\nBy Author: " + this.author);
+		} else {
+			console.log("This note ID does not exist");
+		}
 	}
 
 
 	this.search = function(search_text){
 		textNotFound=false;
 		console.log("Displaying Search results: \n")
-		for (var i=0; i < this.notes.length; i++){
-			if (this.notes[i].includes(search_text)){
-				console.log("Note ID: " + i + "\n" + this.notes[i] + "\n\nBy Author: " + this.author + "\n" + line);
-			} else {
-				textNotFound=true;
+		if (search_text.length > 0){
+			for (var i=0; i < this.notes.length; i++){
+				if (this.notes[i].includes(search_text)){
+					console.log("Note ID: " + i + "\n" + this.notes[i] + "\n\nBy Author: " + this.author + "\n" + line);
+				} else {
+					textNotFound=true;
+				}
 			}
 		}
 		if (textNotFound){
-			console.log("You cant search an empty for");
+			console.log("Sorry, Search term not found!");
 		}
 	}
 
 
 	this.delete = function(note_id){
-		delete this.notes[note_id]
+		if (note_id > -1 && note_id < this.notes.length){
+			this.notes.splice(note_id, 1);
+			console.log("The Note has been deleted!");
+		} else {
+			console.log("Note ID not found!");
+		}
 	}
 
 
 	this.edit = function(note_id, new_content){
-		this.notes[note_id] = new_content;
+		if (note_id > -1 && note_id < this.notes.length){
+			this.notes[note_id] = new_content;
+			console.log("Note has been updated");
+		} else {
+			console.log("Note ID not found!");
+		}
 	}
 
 
-	
+
 }
 
